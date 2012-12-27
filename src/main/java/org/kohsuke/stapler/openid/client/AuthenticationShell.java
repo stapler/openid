52292c04a3ec63c5c71c9ea6f5ed6340430fa299
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-12-27 15:07:24 -0800

    added the client side as well.
    Will rename the module appropriately later

---
+++ src/main/java/org/kohsuke/stapler/openid/client/AuthenticationShell.java
@@ -0,0 +1,75 @@
+package org.kohsuke.stapler.openid.client;
+
+import org.kohsuke.stapler.AttributeKey;
+import org.kohsuke.stapler.HttpResponses;
+import org.kohsuke.stapler.Stapler;
+import org.kohsuke.stapler.StaplerFallback;
+import org.kohsuke.stapler.StaplerRequest;
+import org.openid4java.OpenIDException;
+import org.openid4java.consumer.ConsumerException;
+import org.openid4java.consumer.ConsumerManager;
+import org.openid4java.consumer.InMemoryConsumerAssociationStore;
+import org.openid4java.consumer.InMemoryNonceVerifier;
+import org.openid4java.message.AuthSuccess;
+
+import java.io.IOException;
+
+/**
+ * Protects the delegated URL-bound object via OpenID.
+ *
+ * <p>
+ * If the current request isn't authenticated, send the browser to OpenID authentication sequence
+ * with https://jenkins-ci.org/
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public abstract class AuthenticationShell implements StaplerFallback {
+    private final ConsumerManager manager;
+    private final Object delegate;
+    public final AttributeKey<AuthSuccess> key = AttributeKey.sessionScoped();
+
+    public AuthenticationShell(Object delegate) throws ConsumerException {
+        this.delegate = delegate;
+
+        manager = new ConsumerManager();
+        manager.setAssociations(new InMemoryConsumerAssociationStore());
+        manager.setNonceVerifier(new InMemoryNonceVerifier(5000));
+    }
+
+    public Object getStaplerFallback() {
+        // authenticate the user if needed
+        key.set(currentSession().authenticate());
+        // then fall through
+        return delegate;
+    }
+
+    /**
+     * Maps the login session to URL.
+     */
+    public OpenIdSession getOpenid() {
+        return currentSession();
+    }
+
+    private OpenIdSession currentSession() {
+        StaplerRequest req = Stapler.getCurrentRequest();
+        OpenIdSession o = OpenIdSession.KEY.get(req);
+        if (o==null)
+            try {
+                OpenIdSession.KEY.set(req, o = new OpenIdSession(manager,
+                        getClaimedIdentity(req), req.findAncestor(this).getUrl()+"/openid/"));
+            } catch (OpenIDException e) {
+                throw HttpResponses.error(e);
+            } catch (IOException e) {
+                throw HttpResponses.error(e);
+            }
+        return o;
+    }
+
+    /**
+     * Returns the OpenID that our user is claiming (that we are going to validate.)
+     * In the typical context where this is used, this normally returns a constant
+     * (I don't know what the right term for this, but the generic OpenID identifier
+     * that allows the user to select the actual identifier in the server.)
+     */
+    protected abstract String getClaimedIdentity(StaplerRequest request);
+}
---
+++ src/main/java/org/kohsuke/stapler/openid/client/OpenIdSession.java
@@ -0,0 +1,114 @@
+package org.kohsuke.stapler.openid.client;
+
+import org.kohsuke.stapler.AttributeKey;
+import org.kohsuke.stapler.HttpRedirect;
+import org.kohsuke.stapler.HttpResponse;
+import org.kohsuke.stapler.HttpResponses;
+import org.kohsuke.stapler.Stapler;
+import org.kohsuke.stapler.StaplerRequest;
+import org.openid4java.OpenIDException;
+import org.openid4java.consumer.ConsumerManager;
+import org.openid4java.consumer.VerificationResult;
+import org.openid4java.discovery.DiscoveryInformation;
+import org.openid4java.discovery.Identifier;
+import org.openid4java.message.AuthRequest;
+import org.openid4java.message.AuthSuccess;
+import org.openid4java.message.ParameterList;
+import org.openid4java.message.sreg.SRegRequest;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Captures the in-progress OpenID authentication session and its result.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class OpenIdSession implements Serializable  {
+    // fake serializable just to make Tomcat happy
+    private transient final ConsumerManager manager;
+    private transient final DiscoveryInformation endpoint;
+    private transient String from;
+    private transient final String finishUrl;
+
+    /**
+     * Authenticated identity.
+     */
+    private AuthSuccess identity;
+
+    /**
+     * @param openid
+     *      The identity that the user has claimed, which we are going to validate.
+     * @param thisUrl
+     *      URL that this {@link OpenIdSession} object is mapped to. Used to construct
+     *      the URL to bring the user back to.
+     */
+    public OpenIdSession(ConsumerManager manager, String openid, String thisUrl) throws OpenIDException, IOException {
+        this.manager = manager;
+
+        List discoveries = manager.discover(openid);
+        endpoint = manager.associate(discoveries);
+        finishUrl = thisUrl+"/finishLogin";
+    }
+
+    /**
+     * If the user is already authenticated, return the identity information.
+     * Otherwise start an authentication session (by throwing {@link HttpResponse}.)
+     */
+    public AuthSuccess authenticate() {
+        if (identity==null)
+            commence();     // this redirects the user and will never return
+        return identity;
+    }
+
+    /**
+     * Starts the login session.
+     */
+    public void commence() {
+        try {
+            this.from = Stapler.getCurrentRequest().getRequestURIWithQueryString();
+            final AuthRequest authReq = manager.authenticate(endpoint, finishUrl);
+
+            SRegRequest sregReq = SRegRequest.createFetchRequest();
+            sregReq.addAttribute("fullname", false);
+            sregReq.addAttribute("nickname", true);
+            sregReq.addAttribute("email", false);
+            authReq.addExtension(sregReq);
+
+            String url = authReq.getDestinationUrl(true);
+
+            // remember this in the session
+            KEY.set(this);
+
+            throw new HttpRedirect(url);
+        } catch (OpenIDException e) {
+            throw HttpResponses.error(e);
+        }
+    }
+
+    /**
+     * When the identity provider is done with its thing, the user comes back here.
+     */
+    public HttpResponse doFinishLogin(StaplerRequest request) throws IOException, OpenIDException {
+        // extract the parameters from the authentication process
+        // (which comes in as a HTTP extend from the OpenID provider)
+        ParameterList responselist = new ParameterList(request.getParameterMap());
+
+        // verify the process
+        VerificationResult verification = manager.verify(request.getRequestURLWithQueryString().toString(), responselist, endpoint);
+
+        // examine the verification result and extract the verified identifier
+        Identifier verified = verification.getVerifiedId();
+        if (verified == null)
+            throw HttpResponses.error(500,"Failed to login: " + verification.getStatusMsg());
+
+        this.identity = (AuthSuccess) verification.getAuthResponse();
+
+        return HttpResponses.redirectTo(from);
+    }
+
+    public static final AttributeKey<OpenIdSession> KEY = AttributeKey.sessionScoped();
+
+    private static final long serialVersionUID = 1L;
+}
